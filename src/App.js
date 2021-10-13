b800572ab59abe153b734c0ded7ba94b24e0e028
John Oseni <johnoseni26@gmail.com>
2021-10-13 07:09:15 +0100

    dfijfd

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import './App.css';
 function App() {
   return (
   <div>
-      
+      <div>
+          This is John oseni(Cypher), first desktop application because of mr Akin
+      </div>
+      <div>
+        I used Electron Js to build this, and i can say i have learnt something sir
+      </div>
   </div>
   );
 }
